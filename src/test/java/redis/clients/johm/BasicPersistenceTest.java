47277cec5ae39afbdbc06493c2d20a2df4aacc0e
Anton Bessonov <a.bessonov@placement24.com>
2011-04-17 17:06:37 +0200

    add expire
    Conflicts:
    	src/main/java/redis/clients/johm/JOhm.java
    	src/test/java/redis/clients/johm/BasicPersistenceTest.java

--- src/test/java/redis/clients/johm/BasicPersistenceTest.java
+++ src/test/java/redis/clients/johm/BasicPersistenceTest.java
@@ -232,6 +232,21 @@ public class BasicPersistenceTest extends JOhmTestBase {
 
         JOhm.getAll(Book.class);
     }
+    
+    @Test
+    public void TestExpiration() throws InterruptedException {
+        Book book = new Book();
+        book.setName("expritation title");
+        JOhm.save(book);
+        JOhm.expire(book, 1);
+        Book savedBook = JOhm.get(Book.class, book.getId());
+        assertEquals(book.getName(), savedBook.getName());
+
+        // wait of expire
+        Thread.sleep(1500L);
+        savedBook = JOhm.get(Book.class, book.getId());
+        assertNull(savedBook);
+    }
 
     @Test
     public void testSelectDb() {
